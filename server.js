89afc682629a79a10c26572b8949b4fcd27671e3
Donald Linnell <dlinnell@Donalds-MacBook-Air.local>
2018-04-24 20:59:02 -0500

    Somewhat working

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
+var tableData = require(path.join(__dirname, 'data/tableData'));
+
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 require(path.join(__dirname, 'apiRoutes.js'))(app);
 require(path.join(__dirname, 'htmlRoutes.js'))(app);
