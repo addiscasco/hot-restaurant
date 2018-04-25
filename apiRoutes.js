57126fa06c0a38db46b24d2fd904a0150ba281a1
Donald Linnell <dlinnell@Donalds-MacBook-Air.local>
2018-04-24 19:53:33 -0500

    Continuing routing, setting up JS data

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,7 +1,12 @@
-var tableData = require('/data/tableData')
+var tableData = require('/data/tableData.js')
+var waitingList = require('/data/waitinglistData.js')
 
 module.exports = function(app){
     app.get('/api/tables', function(req, res){
         return res.json(tableData);
     })
-}
+
+    app.get('/api/waitinglist', function(req, res){
+        return res.json(waitingList);
+    })
+};
